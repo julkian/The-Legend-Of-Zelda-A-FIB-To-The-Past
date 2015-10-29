e4e1c5170c00b7d0708f58f532baf5fee8b14ff4
Julkian <meestoy42@gmail.com>
2015-10-29 20:58:23 +0100

    gitignore changed

--- cScene.cpp
+++ cScene.cpp
@@ -10,8 +10,20 @@ cScene::~cScene(void)
 {
 }
 
+/*
+Tmx::Map *loadMap() 
+{
+	Tmx::Map *map = new Tmx::Map();
+	std::string fileName = "./resources/levels/demo.tmx";
+    map->ParseFile(fileName);
+	return map;
+}
+*/
+
 bool cScene::LoadLevel(int level)
 {
+//	Tmx::Map *_map = loadMap();
+
 	bool res;
 	//FILE *fd;
 	char file[16];
@@ -21,22 +33,17 @@ bool cScene::LoadLevel(int level)
 
 	res=true;
 
-	//construyendo nombre de archivo
-	if(level<10) sprintf(file,"%s0%d%s",(char *)FILENAME,level,(char *)FILENAME_EXT);
-	else		 sprintf(file,"%s%d%s",(char *)FILENAME,level,(char *)FILENAME_EXT);
-
-//	fd=fopen(file,"r");
-//	if(fd==NULL) return false;
-
-	/*La asquerosidad hecha codigo*/
-	std::ifstream fd("level01.csv");
+	//archivo con num capas
+	std::ifstream fd(std::string(FILENAME)+ std::string(FILENAME_EXT));
 	std::string str;
-	/*e ya*/
+
+	int numLayers;
+	fd >> numLayers;
 
 	id_DL=glGenLists(1);
 	glNewList(id_DL,GL_COMPILE);
 		glBegin(GL_QUADS);
-			
+		for (int h = 0; h < numLayers; ++h) {
 			for(j=SCENE_HEIGHT-1;j>=0;j--)
 			{
 				px=SCENE_Xo;
--- cScene.h
+++ cScene.h
@@ -4,13 +4,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+//#include "Tmx.h"
 
 #define SCENE_Xo		(2*TILE_SIZE)
 #define SCENE_Yo		TILE_SIZE
 #define SCENE_WIDTH		20
 #define SCENE_HEIGHT	20
 
-#define FILENAME		"level"
+#define FILENAME		"demo"
 #define FILENAME_EXT	".csv"
 
 //#define TILE_SIZE		16
@@ -20,7 +21,7 @@
 #define BLOCK_SIZE	16
 
 /*definiendo variables*/
-#define TILE_ROWS		14	//numero de cuadraicos por fila en el tileset
+#define TILE_ROWS		13	//numero de cuadraicos por fila en el tileset
 #define TILE_COLUMNS	16	//numero de cuadraicos por columna en el tileset
 
 class cScene
@@ -32,6 +33,8 @@ public:
 	bool LoadLevel(int level);
 	void Draw(int tex_id);
 	int *GetMap();
+//	Tmx::Map *loadMap();
+
 
 private:
 	int map[SCENE_WIDTH * SCENE_HEIGHT];	//scene
